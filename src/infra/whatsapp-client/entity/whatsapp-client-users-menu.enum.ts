67a197e0a03d4c33f12c69d1c0c1fcaf3eecba52
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-04-29 19:09:19 -0300

    feat: add WhatsappClientUsersMenu enum and UsersEntity for user management

---
+++ src/infra/whatsapp-client/entity/whatsapp-client-users-menu.enum.ts
@@ -0,0 +1,10 @@
+export enum WhatsappClientUsersMenu {
+  INITIAL = 'initial',
+  LATEST_NEWS = 'latest_news',
+  MATCH_SCHEDULE = 'match_schedule',
+  TROPHY_HISTORY = 'trophy_history',
+  MATCH_ALERTS = 'match_alerts',
+  LIVE_GAME_STATUS = 'live_game_status',
+  FURIA_AI_CHAT = 'furia_ai_chat',
+  OFFICIAL_STORE = 'official_store',
+}
