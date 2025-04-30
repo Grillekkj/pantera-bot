7cbac887a7d42a2489b30ab4d037b98b2023bfcc
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-04-29 21:25:28 -0300

    feat: implement FuriaAiChatService for player and coach retrieval by nickname or name

---
+++ src/modules/furia-ai-chat/service/furia-ai-chat.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FuriaAiChatEntity } from '../entity/furia-ai-chat.entity';
+import { Repository } from 'typeorm';
+import { IFuriaAiChat, IGetByNicknameOrName } from './furia-ai-chat.struct';
+
+@Injectable()
+export class FuriaAiChatService {
+  private readonly LOGGER = new Logger(FuriaAiChatService.name);
+
+  constructor(
+    @InjectRepository(FuriaAiChatEntity)
+    private readonly furiaAiChatRepository: Repository<FuriaAiChatEntity>,
+  ) {}
+
+  public async getByNicknameOrName(
+    data: IGetByNicknameOrName,
+  ): Promise<IFuriaAiChat> {
+    const searchTerm = data.nickname ?? data.name;
+
+    this.LOGGER.log(
+      `Buscando jogador/técnico por nickname ou nome: ${searchTerm}...`,
+    );
+
+    const entity = await this.furiaAiChatRepository.findOne({
+      where: [{ nickname: data.nickname }, { name: data.name }],
+    });
+
+    if (!entity) {
+      this.LOGGER.warn(`Nenhum dado encontrado para ${searchTerm}`);
+      throw new NotFoundException(
+        'Nenhum jogador/técnico com esse nome/nick encontrado',
+      );
+    }
+
+    return entity;
+  }
+}
