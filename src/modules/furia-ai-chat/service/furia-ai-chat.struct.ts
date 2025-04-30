7e00e3e38d616cd08e019b1ad6aec2519b8f19f7
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-04-29 21:37:25 -0300

    refactor: remove IFuriaAiChat interface

--- src/modules/furia-ai-chat/service/furia-ai-chat.struct.ts
+++ src/modules/furia-ai-chat/service/furia-ai-chat.struct.ts
@@ -1,13 +1,3 @@
-export interface IFuriaAiChat {
-  id: string;
-  nickname: string;
-  name: string;
-  game: string;
-  position: string;
-  nationality: string;
-  description: string;
-}
-
 export interface IGetByNicknameOrName {
   nickname?: string;
   name?: string;
