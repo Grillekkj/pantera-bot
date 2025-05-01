5c53f69d01d6f10c492802dbbae6e67212a6b68b
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-05-01 02:16:12 -0300

    feat: implement LatestNewsService for handling news messages

---
+++ src/modules/latest-news/service/latest-news.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SubmenuHandler } from 'src/common/message-handler/submenu-handler.interface';
+import { WhatsappClientService } from 'src/infra/whatsapp-client/service/whatsapp-client.service';
+import { Draft5ScrapperService } from 'src/modules/draft5-scrapper/service/draft5-scrapper.service';
+import { Client } from 'whatsapp-web.js';
+
+@Injectable()
+export class LatestNewsService implements SubmenuHandler {
+  private readonly LOGGER = new Logger(LatestNewsService.name);
+  private readonly WHATSAPP_CLIENT: Client;
+
+  constructor(
+    private readonly whatsappClientService: WhatsappClientService,
+    private readonly draft5ScrapperService: Draft5ScrapperService,
+  ) {
+    this.WHATSAPP_CLIENT = this.whatsappClientService.getClient();
+  }
+
+  async handleMessage(message: any): Promise<void> {
+    await this.whatsappClientService.deleteUser(message.from);
+
+    await this.WHATSAPP_CLIENT.sendMessage(
+      message.from,
+      await this.draft5ScrapperService.getLatestNews(),
+    );
+  }
+}
