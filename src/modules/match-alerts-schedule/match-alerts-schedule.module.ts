605db7ceafb7e89eeb55431d4fae52f0b6614569
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-05-02 02:43:50 -0300

    feat: update MatchAlertsScheduleModule to include TypeOrmModule for MatchAlertsScheduleEntity and ActiveAlerts

--- src/modules/match-alerts-schedule/match-alerts-schedule.module.ts
+++ src/modules/match-alerts-schedule/match-alerts-schedule.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MatchAlertsScheduleService } from './service/match-alerts-schedule.service';
 import { WhatsappClientModule } from 'src/infra/whatsapp-client/whatsapp-client.module';
+import { MatchAlertsScheduleEntity } from './entity/match-alerts-schedule.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GeminiClientModule } from 'src/infra/gemini-client/gemini-client.module';
+import { ActiveAlerts } from './entity/active-alerts.entity';
 
 @Module({
-  imports: [WhatsappClientModule],
+  imports: [
+    TypeOrmModule.forFeature([MatchAlertsScheduleEntity, ActiveAlerts]),
+    WhatsappClientModule,
+    GeminiClientModule,
+  ],
   providers: [MatchAlertsScheduleService],
   exports: [MatchAlertsScheduleService],
 })
